450a5412878c355a0259c2aa92b5ee4c0f351497
jovanwongzixi <77403495+jovanwongzixi@users.noreply.github.com>
2023-06-18 21:37:27 +0800

    Group mobile app files in folder

--- Mobile App/app/(tabs)/index.tsx
+++ Mobile App/app/(tabs)/index.tsx
@@ -11,7 +11,7 @@ export default function TabOneScreen() {
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
