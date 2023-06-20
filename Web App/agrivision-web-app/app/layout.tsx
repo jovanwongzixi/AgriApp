7480a239630c622349b8d5c6df420930e5df86db
jovanwongzixi <77403495+jovanwongzixi@users.noreply.github.com>
2023-06-20 15:44:13 +0800

    Update layout.tsx

--- Web App/agrivision-web-app/app/layout.tsx
+++ Web App/agrivision-web-app/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import AuthContextProvider from './context/auth-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AuthContextProvider>
+          {children}
+        </AuthContextProvider>
+      </body>
     </html>
   )
 }
