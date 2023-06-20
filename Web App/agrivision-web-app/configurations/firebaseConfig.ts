5cd626cd61dd5692c46461537c850ee0fac5cdbd
jovanwongzixi <77403495+jovanwongzixi@users.noreply.github.com>
2023-06-20 15:43:38 +0800

    set up firebaseConfig file

--- Web App/agrivision-web-app/configurations/firebaseConfig.ts
+++ Web App/agrivision-web-app/configurations/firebaseConfig.ts
@@ -1,9 +1,15 @@
-export const firebaseConfig = {
-    apikey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSENGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
+import { initializeApp, getApps } from 'firebase/app';
+
+const firebaseConfig = {
+    apikey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSENGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 }
+
+let firebaseApp = getApps().length === 0 ? initializeApp(firebaseConfig): getApps()[0]
+
+export default firebaseApp
