4e3edb049d26de355e33e8468d02f000baad312e
jovanwongzixi <77403495+jovanwongzixi@users.noreply.github.com>
2023-06-20 15:44:27 +0800

    Create auth-provider.tsx

---
+++ Web App/agrivision-web-app/app/context/auth-provider.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { useContext, createContext, useState, useEffect } from 'react'
+import { onAuthStateChanged, getAuth, User } from 'firebase/auth'
+import firebaseApp from '@/configurations/firebaseConfig'
+
+console.log(firebaseApp)
+
+const auth = getAuth(firebaseApp)
+
+export const AuthContext = createContext({})
+
+export const useAuthContext = () => useContext(AuthContext)
+
+export default function AuthContextProvider({
+    children
+} : {
+    children: React.ReactNode
+}) {
+    const [user, setUser] = useState<User | null>()
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setUser(user)
+            setLoading(false)
+        })
+
+        return () => unsubscribe()
+    }, [])
+
+    return (
+        <AuthContext.Provider value={{ user }}>
+            {loading ? <div>Loading...</div> : children}
+        </AuthContext.Provider>
+    )
+}
