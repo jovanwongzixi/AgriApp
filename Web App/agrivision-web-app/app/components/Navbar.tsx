519aa1fb950dc4b2c6eb449d9d23e91a9ec31192
jovanwongzixi <77403495+jovanwongzixi@users.noreply.github.com>
2023-06-22 00:59:02 +0800

    create navbar with display email

---
+++ Web App/agrivision-web-app/app/components/Navbar.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { User } from 'firebase/auth'
+import { useAuthContext } from '../context/auth-provider'
+import Avatar from './Avatar'
+
+export default function Navbar(){
+    const { user } : { user : User | null } = useAuthContext()
+
+    return(
+        <nav
+            className='justify-between flex flex-row p-5 border-b-[1px]'
+        >
+            <h1>AgriVision</h1>
+            <h2>{ user ? user.email : 'Not Signed In'}</h2>
+            <Avatar src={null}/>
+        </nav>
+    )
+}
