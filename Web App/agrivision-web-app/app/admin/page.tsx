0db7dff32bf837e5a12346e564aae9ee606a3f1b
jovanwongzixi <77403495+jovanwongzixi@users.noreply.github.com>
2023-06-21 19:09:32 +0800

    test for protected page and signout

---
+++ Web App/agrivision-web-app/app/admin/page.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { useEffect } from 'react'
+import { useAuthContext } from '../context/auth-provider'
+import { signOut, getAuth } from 'firebase/auth'
+import { useRouter } from 'next/navigation'
+import { auth } from '../context/auth-provider'
+
+export default function Page(){
+    let { user } = useAuthContext()
+    const router = useRouter()
+    useEffect(() => {
+        console.log(user)
+        if (user == null) router.push('/')
+    }, [user])
+
+    const logOut = async () => {
+        await signOut(auth)
+        console.log('signed out')
+    }
+    return(
+        <>
+            <h1>Logged in!</h1>
+            <button onClick={logOut}>Log Out</button>
+        </>
+        
+    )
+}
