4e9e5de2d5059055047e3193a8f347c59d7186ae
jovanwongzixi <77403495+jovanwongzixi@users.noreply.github.com>
2023-06-23 00:01:05 +0800

    apis to use vercel postgres

---
+++ Web App/agrivision-web-app/app/api/box-to-user/[userid]/route.ts
@@ -0,0 +1,12 @@
+import { db } from '@vercel/postgres'
+import { NextResponse } from 'next/server'
+
+export async function GET(
+    request : Request,
+    { params }: { params: { userid: string } }
+){
+    const client = await db.connect()
+
+    const results = await client.sql`SELECT BoxID FROM BoxToUser WHERE UserID=${params.userid}`
+    return NextResponse.json({ results })
+}
