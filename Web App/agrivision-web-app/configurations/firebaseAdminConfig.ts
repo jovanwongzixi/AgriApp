e54c447d638c08759327a99fd3265e26b5500e12
jovanwongzixi <77403495+jovanwongzixi@users.noreply.github.com>
2023-06-25 00:19:03 +0800

    added firebaseadmin

---
+++ Web App/agrivision-web-app/configurations/firebaseAdminConfig.ts
@@ -0,0 +1,18 @@
+import { initializeApp, getApps, cert } from 'firebase-admin/app'
+
+const firebaseAdminConfig = {
+    // credential:cert({
+    //     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    //     clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+    //     privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+    // })
+    credential:cert(process.env.FIREBASE_ADMIN_SECRET_KEY!)
+}
+
+let firebaseAdminApp = getApps().length === 0 ? initializeApp(firebaseAdminConfig) : getApps()[0]
+export default firebaseAdminApp
+// export function initAdminApp(){
+//     if(getApps().length <= 0){
+//         initializeApp(firebaseAdminConfig)
+//     }
+// }
