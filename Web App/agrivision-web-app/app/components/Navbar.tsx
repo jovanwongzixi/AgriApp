3176b35bcd0ed2bfa0608debb8a01377b9afdc23
jovanwongzixi <77403495+jovanwongzixi@users.noreply.github.com>
2023-06-25 00:18:12 +0800

    Update Navbar.tsx

--- Web App/agrivision-web-app/app/components/Navbar.tsx
+++ Web App/agrivision-web-app/app/components/Navbar.tsx
@@ -3,10 +3,13 @@
 import { User } from 'firebase/auth'
 import { useAuthContext } from '../context/auth-provider'
 import Avatar from './navbar/Avatar'
+import { useEffect } from 'react'
 
 export default function Navbar(){
     const { user } : { user : User | null } = useAuthContext()
-
+    useEffect(()=>{
+        user?.getIdToken().then(val => console.log(`Bearer ${val} ${user?.email}`.split("Bearer ")[1].split(" ")[1]))
+    })
     return(
         <nav
             className='justify-between flex flex-row p-5 border-b-[1px]'
