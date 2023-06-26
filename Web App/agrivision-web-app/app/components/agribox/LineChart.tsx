f5549913cfb7b3ac9335e56d481d779c8a05533c
jovanwongzixi <77403495+jovanwongzixi@users.noreply.github.com>
2023-06-26 15:13:17 +0800

    Create LineChart.tsx
    plot graph using chartjs

---
+++ Web App/agrivision-web-app/app/components/agribox/LineChart.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js'
+
+import { Line } from 'react-chartjs-2'
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+)
+
+const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top' as const,
+        },
+        title: {
+            display: true,
+            text: 'Chart.js Line Chart',
+        },
+    },
+}
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+    labels,
+    datasets: [
+        {
+            label: 'Dataset 1',
+            data: [200, 800, 600, 300, 1000, -1000, 400,],
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        },
+        {
+            label: 'Dataset 2',
+            data: [100, 300, -600, 900, -1000, 1000, 200,],
+            borderColor: 'rgb(53, 162, 235)',
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        },
+    ],
+}
+
+export default function LineChart(){
+    return(
+        <Line options={options} data={data} />
+    )
+}
