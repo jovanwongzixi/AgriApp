438b33d594aa7855cecb0f77e3eb3fd1797294dc
jovanwongzixi <77403495+jovanwongzixi@users.noreply.github.com>
2023-06-26 15:14:38 +0800

    update agribox data vis page
    includes check if user has box

--- Web App/agrivision-web-app/app/[userid]/agribox/[boxid]/page.tsx
+++ Web App/agrivision-web-app/app/[userid]/agribox/[boxid]/page.tsx
@@ -1,7 +1,25 @@
-export default async function Page({ params }: { params : { boxid: string }}){
+import { db } from '@vercel/postgres'
+
+import Error from 'next/error'
+import LineChart from '@/app/components/agribox/LineChart'
+
+async function checkUserHasBox(boxid: string, userid: string){
+    const client = await db.connect()
+    const result = await client.sql`SELECT * FROM boxtouser WHERE boxid=${boxid} AND userid=${userid}`
+    if (result.rows.length === 0) return false
+    return true
+}
+
+export default async function Page({ params }: { params : { boxid: string, userid: string }}){
+    // check working
+    // let hasBox = true
+    // if (!await checkUserHasBox(params.boxid, params.userid)) hasBox = false
+    
     return(
         <div>
+            {/* {hasBox === false ? <>Do not own box</> :<></>} */}
             <h1>{params.boxid}</h1>
+            <LineChart />
         </div>
     )
 }
