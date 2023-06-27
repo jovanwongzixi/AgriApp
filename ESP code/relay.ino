335af6a9d157eb7a846b4f5307955b57a075c1ec
jovanwongzixi <77403495+jovanwongzixi@users.noreply.github.com>
2023-06-28 00:47:06 +0800

    Update relay.ino

--- ESP code/relay.ino
+++ ESP code/relay.ino
@@ -1,21 +1,22 @@
-const int relay = 26; //fan
+const int fan = 26; //fan
 const int pump = 27;
 void setup() {
   Serial.begin(115200);
-  pinMode(relay, OUTPUT);
+  pinMode(fan, OUTPUT);
+  pinMode(pump, OUTPUT);
 }
 
 void loop() {
   // Normally Open configuration, send LOW signal to let current flow
   // (if you're usong Normally Closed configuration send HIGH signal)
-  digitalWrite(relay, LOW);
+  digitalWrite(fan, LOW);
   digitalWrite(pump, LOW);
   Serial.println("Current Flowing");
   delay(5000); 
   
   // Normally Open configuration, send HIGH signal stop current flow
   // (if you're usong Normally Closed configuration send LOW signal)
-  digitalWrite(relay, HIGH);
+  digitalWrite(fan, HIGH);
   digitalWrite(pump, HIGH);
   Serial.println("Current not Flowing");
   delay(5000);
