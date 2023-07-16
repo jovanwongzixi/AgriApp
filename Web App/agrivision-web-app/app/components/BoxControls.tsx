d1b592675ca58806b58b9f9272cc29d8808f7ff5
jovanwongzixi <77403495+jovanwongzixi@users.noreply.github.com>
2023-07-17 00:31:37 +0800

    Update BoxControls.tsx
    added indiv switch controls

--- Web App/agrivision-web-app/app/components/BoxControls.tsx
+++ Web App/agrivision-web-app/app/components/BoxControls.tsx
@@ -1,18 +1,41 @@
 'use client'
 
+import { useState } from 'react'
 import type { CompatClient } from '@stomp/stompjs'
 
-export default function BoxControls({ client } : {client : CompatClient | undefined}){
+function IndividualControl({ name, state, onChange } : { name: string, state: boolean, onChange : (e: React.ChangeEvent<HTMLInputElement>) => void }){
+    // const [controlStatus, useControlStatus] = useState<boolean>()
     return(
-        <div>
-            <div className='flex flex-row'>
-                <p>Pump</p>
-                <label className="relative inline-flex items-center cursor-pointer">
-                    <input type="checkbox" value="" className="sr-only peer" />
-                    <div className="w-11 h-6 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-800 rounded-full peer bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                    <span className="ml-3 text-sm font-medium text-gray-300">Toggle me</span>
-                </label>
-            </div>
+        <div className='flex flex-row'>
+            <p>{name}</p>
+            <label className="relative inline-flex items-center cursor-pointer">
+                <input type="checkbox" value="" name={name} className="sr-only peer" checked={state} onChange={onChange}/>
+                <div className="w-11 h-6 peer-focus:outline-none rounded-full peer bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white hover:after:ring-4 hover:after:ring-gray-400/60 after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white peer-checked:after:bg-[#6AFF67] after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600"></div>
+                {/* <span className="ml-3 text-sm font-medium text-gray-300">Toggle me</span> */}
+            </label>
+        </div>
+    )
+}
+
+export default function BoxControls({ client } : {client : CompatClient | undefined}){
+    const [controlsStatus, useControlsStatus] = useState({
+        Fan: false,
+        Pump: false
+    })
+
+    const onChangeControl = (e : React.ChangeEvent<HTMLInputElement>) => {
+        const {name, checked} = e.target
+        useControlsStatus(prevVal => {
+            return {
+                ...prevVal,
+                [name]: checked
+            }
+        })
+    }
+    return(
+        <div className='rounded-2xl border-[#9E9E9E] border-[1px] p-2 max-w-fit'>
+            <IndividualControl name='Fan' state={controlsStatus['Fan']} onChange={onChangeControl}/>
+            <IndividualControl name='Pump' state={controlsStatus['Pump']} onChange={onChangeControl}/>
         </div>
     )
 }
