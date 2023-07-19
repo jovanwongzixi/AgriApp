0606f38bba5d6c30b3e867760ed8d3db0726cbfa
jovanwongzixi <77403495+jovanwongzixi@users.noreply.github.com>
2023-07-19 17:42:54 +0800

    added settings page

---
+++ Web App/agrivision-web-app/app/components/settings/AgriCloudSettings.tsx
@@ -0,0 +1,25 @@
+import TestClient from "./TestClient"
+
+export default async function AgriCloudSettings({ userid }: { userid: string }){
+    const res = await fetch(`http://localhost:3000/api/agricloud/check-permission?userid=${userid}`)
+    const { result } = await res.json()
+    const {shareData, premium} = result
+
+    return(
+        <div>
+            <p className='font-bold tracking-tighter text-5xl mb-5'>AgriCloud</p>
+            <p className='text-[#DBDBDB] mb-5'>Manage AgriCloud permissions and billing</p>
+            <div className='border rounded-xl border-[#B2B2B2] py-3 px-4 mb-6'>
+                <p className='font-medium text-lg mb-2'>Payment is in demo mode.</p>
+                <p>Our current payment system is using a Stripe test environment. You can find a list of test card numbers on the Stripe docs.</p>
+            </div>
+            <div className='border rounded-xl border-[#B2B2B2] py-3 px-4'>
+                <p className='font-semibold tracking-tight text-xl mb-2'>Access</p>
+                <div className='text-[#DBDBDB] mb-2'>
+                    <p>AgriCloud Access {(premium || shareData) ? 'permitted': 'denied'}</p>
+                    <p>You are currently on {premium ? 'premium': 'free'} tier</p>
+                </div>
+            </div>
+        </div>
+    )
+}
