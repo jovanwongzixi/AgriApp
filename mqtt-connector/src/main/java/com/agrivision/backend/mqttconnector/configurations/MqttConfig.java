bdd986724d8951076187d5af817da69068ba9ee4
jovanwongzixi <77403495+jovanwongzixi@users.noreply.github.com>
2023-07-19 12:05:34 +0800

    Update MqttConfig.java
    remove import of properties

--- mqtt-connector/src/main/java/com/agrivision/backend/mqttconnector/configurations/MqttConfig.java
+++ mqtt-connector/src/main/java/com/agrivision/backend/mqttconnector/configurations/MqttConfig.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Properties;
 import java.util.UUID;
 @Configuration
 public class MqttConfig {
