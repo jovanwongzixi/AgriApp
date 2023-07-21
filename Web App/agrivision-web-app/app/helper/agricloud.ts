3ecdbd7bfa316750e2d299ab9f86faf5a0640d15
jovanwongzixi <77403495+jovanwongzixi@users.noreply.github.com>
2023-07-21 23:47:11 +0800

    Update agricloud.ts
    added stripeCustomerID as return field

--- Web App/agrivision-web-app/app/helper/agricloud.ts
+++ Web App/agrivision-web-app/app/helper/agricloud.ts
@@ -2,9 +2,9 @@ import { VercelPoolClient, db } from '@vercel/postgres'
 
 export async function checkAgricloudPermission(userid: string){
     const client = await db.connect()
-    const result = client.sql`SELECT * FROM users WHERE userid=${userid} AND share_data=TRUE`
-    const [share_data, premium] = await Promise.all([result, checkPremiumNeedClient(client, userid)])
-    if ((share_data.rows.length > 0) || premium ) return {shareData: share_data.rows!.at(0)?.share_data, premium: premium}
+    const result = client.sql`SELECT * FROM users WHERE userid=${userid}`
+    const [share_data, { premium }] = await Promise.all([result, checkPremiumNeedClient(client, userid)])
+    if ((share_data.rows.length > 0) || premium ) return {shareData: share_data.rows?.at(0)?.share_data as boolean, premium: premium}
     return null
 }
 
@@ -20,8 +20,8 @@ export async function checkPremium(userid: string){
 }
 
 async function checkPremiumNeedClient(client: VercelPoolClient, userid: string){
-    const result = await client.sql`SELECT endperiod FROM subscriptions WHERE userid=${userid}`
-    if (result.rowCount === 0) return false
-    if (result.rows.at(0)!.endperiod?.getTime() + 86_400_000 > Date.now()) return true
-    return false
+    const result = await client.sql`SELECT endperiod, stripe_customerid FROM subscriptions WHERE userid=${userid}`
+    if (result.rowCount === 0) return {premium: false, stripeCustomerId: ''}
+    if (result.rows.at(0)!.endperiod?.getTime() + 86_400_000 > Date.now()) return {premium: true, stripeCustomerId: result.rows.at(0)!.stripe_customerid as string}
+    return {premium: false, stripeCustomerId: ''}
 }
