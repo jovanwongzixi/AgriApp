4bca972e6d62ac4c754ce168ad56c981a1b8eb0b
jovanwongzixi <77403495+jovanwongzixi@users.noreply.github.com>
2023-07-21 18:09:17 +0800

    added stripe api routes

---
+++ Web App/agrivision-web-app/app/api/stripe/route.ts
@@ -0,0 +1,43 @@
+import { convertEmailToUserid } from "@/app/helper/functions"
+import { NextResponse } from "next/server"
+import { stripe } from "@/app/configurations/stripe"
+import { cookies } from "next/headers"
+import { auth } from "firebase-admin"
+import { getAuth } from "firebase/auth"
+const billingUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/farmboys2000/settings`
+
+export async function GET(request: Request){
+    const session = cookies().get('session')?.value
+    if(!session) return NextResponse.json({error: 'No session!'}, {status: 403})
+
+    const verifiedSession = await auth().verifySessionCookie(session)
+    if(!verifiedSession) return NextResponse.json({error: 'Invalid session!'}, {status: 403})
+
+    const email = verifiedSession.email
+    const userid = convertEmailToUserid(email)
+
+    if(userid === '') return NextResponse.json({error: 'No user!'}, {status: 403})
+    // return NextResponse.json({verifiedSession})
+    try{
+        const stripeSession = await stripe.checkout.sessions.create({
+            success_url: billingUrl,
+            cancel_url: billingUrl,
+            payment_method_types: ["card"],
+            mode: "subscription",
+            billing_address_collection: "auto",
+            customer_email: email,
+            line_items: [
+                {
+                    price: process.env.STRIPE_PRICE_API_ID || '',
+                    quantity: 1,
+                },
+            ],
+            metadata: {
+                userId: userid,
+            },
+        })
+        return new Response(JSON.stringify({ url: stripeSession.url }))
+    } catch(e){
+        return NextResponse.json( {error: e}, { status: 500, 'statusText': JSON.stringify(e) })
+    }
+}
--- Web App/agrivision-web-app/app/api/webhooks/stripe/route.ts
+++ Web App/agrivision-web-app/app/api/webhooks/stripe/route.ts
@@ -1,5 +1,5 @@
 import { headers } from 'next/headers'
-
+import { db } from '@vercel/postgres'
 import Stripe from 'stripe'
 import { stripe } from '@/app/configurations/stripe'
 
@@ -20,11 +20,14 @@ export async function POST(request: Request){
     }
     const session = event.data.object as Stripe.Checkout.Session
     
+    const dbClient = await db.connect()
+
     if (event.type === "checkout.session.completed") {
         // Retrieve the subscription details from Stripe.
         const subscription = await stripe.subscriptions.retrieve(
           session.subscription as string
         )
+        await dbClient.sql`INSERT INTO subscriptions VALUES(${subscription.id}, ${subscription.customer as string}, ${subscription.current_period_end})`
     }
     return new Response(null, {status: 200})
 }
