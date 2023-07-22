8e43c75921dbabaf31d064aaf8d4832ca10c1153
jovanwongzixi <77403495+jovanwongzixi@users.noreply.github.com>
2023-07-22 23:47:20 +0800

    Update Avatar.tsx
    added drop down with settings and logout button

--- Web App/agrivision-web-app/app/components/navbar/Avatar.tsx
+++ Web App/agrivision-web-app/app/components/navbar/Avatar.tsx
@@ -1,8 +1,11 @@
 'use client'
 
 import Image from 'next/image'
-import Link from 'next/link'
 import { useRouter } from 'next/navigation'
+import { auth } from '@/app/context/auth-provider'
+import { signOut } from 'firebase/auth'
+import { BaseSyntheticEvent, useState } from 'react'
+import { twClassMerge } from '@/app/helper/functions'
 
 export default function Avatar({ 
     src,
@@ -13,10 +16,41 @@ export default function Avatar({
 }
     ){
         const router = useRouter()
+        const [dropdownOpen, setDropdownOpen] = useState(false)
+        window.onclick = function(e: MouseEvent){
+            // console.log(e.target)
+            if(e.target !== document.getElementById('dropdown-btn')){
+                setDropdownOpen(false)
+            }
+        }
+        const onClickDropDown = (e: BaseSyntheticEvent) => {
+            // console.log(e)
+            setDropdownOpen(val => !val)
+        }
+
+        const logOut = async () => {
+            const signOutClient = signOut(auth)
+            const signOutServer = async () => {
+                const res = await fetch(`${process.env.BASE_URL}/api/logout`, {
+                    method: 'GET'
+                })
+                return res.status
+            }
+            await Promise.all([signOutClient, signOutServer()])
+            console.log('signed out')
+            router.push('/')
+        }
 
         return(
             <div className='flex flex-row text-white'>
-                <h2>{userid !== '' ? <Link href={'/'}>{userid}</Link> : 'Not Signed In'}</h2>
+                <div className='relative inline-block'>
+                    <button id='dropdown-btn' onClick={onClickDropDown}>{userid}</button>
+                    <div className={twClassMerge('peer absolute py-1 bg-[#102507] min-w-fit overflow-auto z-10 rounded-lg peer-focus:block', !dropdownOpen && 'hidden')}>
+                        <button className='py-1 w-full hover:bg-gray-600' onClick={() => router.push('/settings')}>Settings</button>
+                        {/* <hr /> */}
+                        <button className='py-1 hover:bg-gray-600 w-full text-red-700' onClick={logOut}>Logout</button>
+                    </div>
+                </div>
                 <Image
                     onClick={() => router.push(`/${userid}`)}
                     className='rounded-full cursor-pointer'
@@ -26,6 +60,5 @@ export default function Avatar({
                     src={src || '/placeholder.jpg'}
                 />
             </div>
-            
     )
 }
