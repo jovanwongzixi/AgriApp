c13708957660f1a2e0f9d6d0deea52bda24e674e
jovanwongzixi <77403495+jovanwongzixi@users.noreply.github.com>
2023-07-22 18:23:04 +0800

    Delete layout.tsx

--- Web App/agrivision-web-app/app/agribox/[boxid]/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function Layout({ children, cv } : { children: React.ReactNode, cv: React.ReactNode}){
-    return(
-        <div className='h-full w-full'>
-            {children}
-            {/* {cv} */}
-        </div>
-    )
-}
