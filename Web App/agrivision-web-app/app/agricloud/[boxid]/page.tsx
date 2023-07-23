8e7c002088be43c42ce49f795c5a897e25255325
jovanwongzixi <77403495+jovanwongzixi@users.noreply.github.com>
2023-07-23 16:29:57 +0800

    updated agricloud pages

--- Web App/agrivision-web-app/app/agricloud/[boxid]/page.tsx
+++ Web App/agrivision-web-app/app/agricloud/[boxid]/page.tsx
@@ -1,9 +1,13 @@
-export default function Page({ params }: { params : { boxid: string, userid: string }}){
-    
+import HistoricalData from "@/app/components/HistoricalData"
+
+export default async function Page({ params }: { params : { boxid: string }}){
+    const period = '1'
+    const res = await fetch(`${process.env.BASE_URL}/api/line-chart-data?boxid=${params.boxid}&period=${period}`)
+    const {labels, values, error} = await res.json()
+    if(res.status !== 200) throw new Error(error)
     return(
-        <div>
-            {/* <DataVis boxid={params.boxid} controllable={false}/> */}
-            {/* {hasBox === false ? <>Do not own box</> :<></>} */}
+        <div className='px-28 h-[calc(100vh-130px)] pt-4'>
+            <HistoricalData labels={labels} values={values} boxid={params.boxid}/>
         </div>
     )
 }
