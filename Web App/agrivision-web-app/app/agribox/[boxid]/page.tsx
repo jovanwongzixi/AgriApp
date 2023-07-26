599a53658680e21429e82ce3514d8cfa6f1a58b8
jovanwongzixi <77403495+jovanwongzixi@users.noreply.github.com>
2023-07-26 23:18:13 +0800

    added download data link

--- Web App/agrivision-web-app/app/agribox/[boxid]/page.tsx
+++ Web App/agrivision-web-app/app/agribox/[boxid]/page.tsx
@@ -1,7 +1,11 @@
 import Link from 'next/link'
 import WebSocketClient from '@/app/components/WebSocketClient'
 import HistoricalData from '@/app/components/HistoricalData'
+import { getDownloadURL, getStorage, ref } from 'firebase/storage'
+import firebaseApp from '@/app/configurations/firebaseConfig'
 
+const storage = getStorage(firebaseApp)
+const storageRef = ref(storage, `gs://${process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET}/readings/box1_readings.csv`)
 export default async function Page({ 
     params,
     searchParams,
@@ -19,6 +23,8 @@ export default async function Page({
     const res = await fetch(`${process.env.BASE_URL}/api/line-chart-data?boxid=${params.boxid}&period=${period}`)
     const {labels, values, error} = await res.json()
     if(res.status !== 200) throw new Error(error)
+
+    const fileURL = await getDownloadURL(storageRef)
     return(
         <div className='px-28 h-[calc(100vh-130px)]'>
             <div className='text-white flex flex-row justify-between my-3 items-center'>
@@ -28,6 +34,11 @@ export default async function Page({
                     className='border rounded-2xl bg-white hover:bg-[#D9D9D9] text-black text-center max-h-fit p-2'
                     href={`/agribox/${params.boxid}/cv`}
                 ><p>View Computer</p><p>Vision</p></Link>
+                <a 
+                    className='border rounded-2xl bg-white hover:bg-[#D9D9D9] text-black text-center max-h-fit p-2' 
+                    download 
+                    href={fileURL}
+                >Download Historical Data</a>
             </div>
             <HistoricalData labels={labels} values={values} boxid={params.boxid}/>
             {/* <DataVis boxid={params.boxid} controllable/> */}
--- Web App/agrivision-web-app/app/agricloud/[boxid]/page.tsx
+++ Web App/agrivision-web-app/app/agricloud/[boxid]/page.tsx
@@ -1,12 +1,26 @@
 import HistoricalData from "@/app/components/HistoricalData"
+import { getDownloadURL, getStorage, ref } from 'firebase/storage'
+import firebaseApp from '@/app/configurations/firebaseConfig'
+
+const storage = getStorage(firebaseApp)
+const storageRef = ref(storage, `gs://${process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET}/readings/box1_readings.csv`)
 
 export default async function Page({ params }: { params : { boxid: string }}){
     const period = '1'
     const res = await fetch(`${process.env.BASE_URL}/api/line-chart-data?boxid=${params.boxid}&period=${period}`)
     const {labels, values, error} = await res.json()
     if(res.status !== 200) throw new Error(error)
+    
+    const fileURL = await getDownloadURL(storageRef)
     return(
-        <div className='px-28 h-[calc(100vh-130px)] pt-4'>
+        <div className='px-28 h-[calc(100vh-130px)]'>
+            <div className='my-3 flex flex-row justify-end'>
+                <a 
+                    className='border rounded-2xl bg-white hover:bg-[#D9D9D9] text-black text-center p-2' 
+                    download 
+                    href={fileURL}
+                >Download Historical Data</a>
+            </div>
             <HistoricalData labels={labels} values={values} boxid={params.boxid}/>
         </div>
     )
