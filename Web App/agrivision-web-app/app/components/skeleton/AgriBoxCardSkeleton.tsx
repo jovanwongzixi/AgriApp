eb503e5a7b8f2b920a654765ad9e2419dd693774
jovanwongzixi <77403495+jovanwongzixi@users.noreply.github.com>
2023-07-27 21:39:44 +0800

    Update AgriBoxCardSkeleton.tsx

--- Web App/agrivision-web-app/app/components/skeleton/AgriBoxCardSkeleton.tsx
+++ Web App/agrivision-web-app/app/components/skeleton/AgriBoxCardSkeleton.tsx
@@ -14,7 +14,7 @@ export default function AgriBoxCardSkeleton(){
             >
                 <div className='h-full w-full object-cover animate-pulse bg-[#1C2E18]'/>
             </div>
-            <div className='h-4 w-36 rounded-full bg-[#1C2E18] mt-3' />
+            <div className='h-4 w-36 rounded-full animate-pulse bg-[#1C2E18] mt-3' />
         </div>
     )
 }
